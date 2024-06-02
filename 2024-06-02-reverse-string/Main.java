06daa7db04a76d10d25bee38412bc6443ac2b82e
NAVEEN KUMAR G <57283435+naveenkumar-4@users.noreply.github.com>
2024-06-02 16:15:39 +0530

    Add files via upload

---
+++ 2024-06-02-reverse-string/Main.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Main{
+    public static void reverseString(char[] s){
+        int n = s.length;
+        int start = 0, end = n-1;
+        while(start <= end){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--; 
+        }
+    }
+
+    public static void main(String args[]){
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+}
