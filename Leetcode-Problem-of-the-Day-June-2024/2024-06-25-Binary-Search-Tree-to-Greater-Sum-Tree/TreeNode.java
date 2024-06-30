6cd8e86b28aec78ba9c4136647cddf156ea0b644
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-07-01 00:01:17 +0530

    Binary Search Tree to Greater Sum Tree

---
+++ Leetcode-Problem-of-the-Day-June-2024/2024-06-25-Binary-Search-Tree-to-Greater-Sum-Tree/TreeNode.java
@@ -0,0 +1,59 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    private static int sum = 0;
+
+    public static TreeNode bstToGst(TreeNode root) {
+        if (root != null) {
+            bstToGst(root.right);
+            sum += root.val;
+            root.val = sum;
+            bstToGst(root.left);
+        }
+        return root;
+    }
+
+    public static void printTree(TreeNode root) {
+        if (root != null) {
+            printTree(root.left);
+            System.out.print(root.val + " ");
+            printTree(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(2);
+        root.left.right.right = new TreeNode(3);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(7);
+        root.right.right.right = new TreeNode(8);
+
+        // Transform to Greater Sum Tree
+        TreeNode res = bstToGst(root);
+
+        // Print the transformed tree (in-order)
+        System.out.println("Greater Sum Tree (in-order):");
+        printTree(res);
+    }
+}
