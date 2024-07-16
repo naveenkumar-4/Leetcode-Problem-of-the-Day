5640c099c5df28d0a76e59f693eea7fc0f6a695f
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-07-16 06:29:53 +0530

    Reverse Substrings Between Each Pair of Parentheses

---
+++ Leetcode-Problem-of-the-Day-July-2024/2024-07-11-Reverse-Substrings-Between-Each-Pair-of-Parentheses/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Solution {
+    public static String reverseParentheses(String s) {
+        int n = s.length();
+        int[] index_pairs = new int[n];
+        Deque<Integer> stack_start_ind = new LinkedList<>();
+
+        for (int char_ind = 0; char_ind < n; ++char_ind) {
+            char char_s = s.charAt(char_ind);
+            if (char_s == '(') {
+                stack_start_ind.push(char_ind);
+            } else if (char_s == ')') {
+                int start_ind = stack_start_ind.pop();
+                index_pairs[char_ind] = start_ind;
+                index_pairs[start_ind] = char_ind;
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        int cur_ind = 0;
+        int cur_dir = 1;
+
+        while (cur_ind < n) {
+            char char_s = s.charAt(cur_ind);
+            if (char_s == '(' || char_s == ')') {
+                cur_ind = index_pairs[cur_ind];
+                cur_dir *= -1;
+            } else {
+                res.append(char_s);
+            }
+            cur_ind += cur_dir;
+        }
+
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "(u(love)i)";
+        String res = reverseParentheses(s);
+        System.out.println(res);
+    }
+}
